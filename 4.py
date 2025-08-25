ec0cd8e492bf3ac007187aa9928e08b6dd19bee7
sumantth kona <skona5@gitam.in>
2025-08-25 12:33:43 +0000

    Added 3.py, 4.py and their input files

---
+++ 4.py
@@ -0,0 +1,5 @@
+with open("input2.txt", "r") as f:
+    for line in f:
+        a, b, c, t = map(float, line.strip().split())
+        T = a * t**2 + b * t + c
+        print(f"a={a}, b={b}, c={c}, t={t} -> T={T:.2f}°C")
